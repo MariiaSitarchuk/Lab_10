3d2abc916c8e598114921c2623396cc8d1b2248a
MariiaSitarchuk <92577191+MariiaSitarchuk@users.noreply.github.com>
2022-11-24 10:04:24 +0200

    Update Main.java

--- src/main/java/adapter/task_2/Main.java
+++ src/main/java/adapter/task_2/Main.java
@@ -3,10 +3,10 @@ package adapter.task_2;
 import java.time.LocalDate;
 
 public class Main {
-    public User login(String loginMethod) {
-        if (loginMethod.equals("twitter")) {
+    public User login(String user_login) {
+        if (user_login.equals("twitter")) {
             return new TwitterClientWrapper(new TwitterUser("dobosevych", Country.Ukraine, LocalDate.of(2021, 11,11)));
-        } else if (loginMethod.equals("facebook")) {
+        } else if (user_login.equals("facebook")) {
             return new FacebookClientWrapper(new FacebookUser("dobosevych", Country.Ukraine, LocalDate.of(2021, 11,11)));
         }
         return null;
