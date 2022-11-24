6354747ce77dc8f9d12c789c576be2ddab923e08
MariiaSitarchuk <92577191+MariiaSitarchuk@users.noreply.github.com>
2022-11-24 10:16:17 +0200

    Update and rename БазаДаних.java to DataBase.java

---
+++ src/main/java/adapter/task_3/DataBase.java
@@ -0,0 +1,11 @@
+package adapter.task_3;
+
+public class DataBase {
+    public String getUserInfo() {
+        return "hello";
+    }
+
+    public String getStaticInfo() {
+        return "hello2";
+    }
+}
