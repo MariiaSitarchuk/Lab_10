6d6b8b3c265ce186169d709b2da41e8b5d852622
MariiaSitarchuk <92577191+MariiaSitarchuk@users.noreply.github.com>
2022-11-24 10:54:08 +0200

    Update ReportBuilder.java

--- src/main/java/adapter/task_3/ReportBuilder.java
+++ src/main/java/adapter/task_3/ReportBuilder.java
@@ -1,8 +1,8 @@
 package adapter.task_3;
 
 public class ReportBuilder {
-    private БазаДаних db;
-    public ReportBuilder(БазаДаних db) {
-        this.db = db;
+    private DataBase database;
+    public ReportBuilder(DataBase database) {
+        this.database = database;
     }
 }
