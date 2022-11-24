6f8c3758a6aa519647900174b0ec2e51aa0dcd73
MariiaSitarchuk <92577191+MariiaSitarchuk@users.noreply.github.com>
2022-11-24 10:23:36 +0200

    Update Main.java

--- src/main/java/adapter/task_3/Main.java
+++ src/main/java/adapter/task_3/Main.java
@@ -2,10 +2,10 @@ package adapter.task_3;
 
 public class Main {
     public static void main(String[] args) {
-        БазаДаних db = new БазаДаних();
-        Авторизація авторизація = new Авторизація();
-        if (авторизація.авторизуватися(db)) {
-            ReportBuilder br = new ReportBuilder(db);
+        DataBase database = new DataBase();
+        Authorization authorization = new Authorization();
+        if (authorization.log_in(database)) {
+            ReportBuilder br = new ReportBuilder(database);
         }
     }
 }
